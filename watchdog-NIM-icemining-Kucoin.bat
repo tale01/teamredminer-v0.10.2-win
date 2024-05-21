1ddbfc9f777b15cd8d035505c7dadf76d8d11bf9
tale01 <31344227+tale01@users.noreply.github.com>
2024-05-21 10:53:42 +0700

    Add files via upload

---
+++ watchdog-NIM-icemining-Kucoin.bat
@@ -0,0 +1,3 @@
+timeout /t 5
+start "" cmd /c "echo Hello NH - %date%-%time%!&echo(&pause"
+start zz_NIM-icemining-Kucoin.bat
---
+++ watchdogNH.bat
@@ -0,0 +1,3 @@
+timeout /t 5
+start "" cmd /c "echo Hello NH - %date%-%time%!&echo(&pause"
+start zz_zil-Kawpow-NH.bat
